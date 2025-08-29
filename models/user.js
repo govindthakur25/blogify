af368652788a197a977546624ca0a65bc438a7fe
Govind Thakur <govindthakur25@gmailcom>
2025-08-29 06:20:23 +0530

    fix: Add signin and signup components

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+import { createHmac, randomBytes } from "node:crypto";
 import { Schema, model } from "mongoose";
 
 const userSchema = Schema(
@@ -18,9 +19,33 @@ const userSchema = Schema(
       type: String,
       required: true,
     },
+    profileImage: {
+      type: String,
+      default: "/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+  next();
+});
 
 const User = model("user", userSchema);
 
--- routes/server/user.js
+++ routes/server/user.js
@@ -1,13 +1,36 @@
-import express from "express";
+import { createHmac, randomBytes } from "node:crypto";
 
+import express from "express";
+import { User } from "../../models/user.js";
 const userRoute = express.Router();
 
-userRoute.get("/signup", (req, res) => {
-  res.render("signup");
+userRoute.post("/signup", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  await User.create({
+    fullName,
+    email,
+    password,
+  });
+  res.redirect("/");
 });
 
-userRoute.get("/signin", (req, res) => {
-  res.render("signin");
+userRoute.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    email,
+  });
+  if (!user) {
+    res.redirect("/user/signup", { error: "Email is not registered!" });
+  }
+  const { salt } = user;
+  const hashIncomingPassword = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+  if (hashIncomingPassword !== user.password) {
+    res.redirect("/user/signin", { error: "Incorrect username or password!" });
+  }
+  // assign JWT token
+  res.redirect("/");
 });
 
 export { userRoute };
